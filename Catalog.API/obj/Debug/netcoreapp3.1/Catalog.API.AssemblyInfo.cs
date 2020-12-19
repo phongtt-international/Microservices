a501a4f03112c7d0f20def0f58e589f525381fd9
phongtt <phongtt.international@gmail.com>
2020-12-19 19:24:12 +0700

    Initial BasketAPI and Create Entities BasketCart

--- Catalog.API/obj/Debug/netcoreapp3.1/Catalog.API.AssemblyInfo.cs
+++ Catalog.API/obj/Debug/netcoreapp3.1/Catalog.API.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("308c8dd1-3ba6-42ad-94ec-0f5354b7a20c")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
